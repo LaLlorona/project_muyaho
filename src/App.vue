12cbc40b3020fba371ec9139b5219494dda19ef0
lallorona <chachaami66@gmail.com>
2021-03-03 14:38:34 +0000

    asdf

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<v-app>
+		<v-app-bar app color="primary" dark>
+			<v-app-bar-nav-icon></v-app-bar-nav-icon>
+			<v-toolbar-title>multi pages</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<v-btn>asdfasdf </v-btn>
+			<v-btn icon>
+				<v-icon>mdi-dots-vertical</v-icon>
+			</v-btn>
+		</v-app-bar>
+		<v-content>
+			<router-view></router-view>
+		</v-content>
+
+		<v-footer color="secondary" fixed dark>
+			<div class="mx-auto">CODE-DESIGN.web.app</div>
+		</v-footer>
+	</v-app>
+</template>
---
+++ src/components/main_page.vue
@@ -0,0 +1,34 @@
+<template>
+	<v-container>
+		<v-content>
+			<div class="text-center display-3 my-4">this is the main page</div>
+			<v-row>
+				<v-col offset-sm="1" sm="10">
+					<v-text-field label="title" v-model="sTitle"></v-text-field>
+				</v-col>
+			</v-row>
+
+			<div class="text-center">
+				<v-btn large class="mt-5" color="purple" dark @click="fnSetTitle">
+					confirm
+				</v-btn>
+			</div>
+		</v-content>
+	</v-container>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			sTitle: this.$store.getters.fnGetData,
+		};
+	},
+	methods: {
+		fnSetTitle() {
+			this.$store.commit('fnSetData', this.sTitle);
+			this.$router.push('/sub');
+		},
+	},
+};
+</script>
