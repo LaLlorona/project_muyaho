04c310a6bce88e302d29236d7256fef9e4815c03
lallorona <chachaami66@gmail.com>
2021-03-11 02:20:56 +0000

    comment function almost complete

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,14 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+	theme: {
+		themes: {
+			light: {
+				primary: '#79B791',
+				secondary: '#ABD1B5',
+				accent: '#EDF4ED',
+				error: '#ffffff',
+			},
+		},
+	},
 });
